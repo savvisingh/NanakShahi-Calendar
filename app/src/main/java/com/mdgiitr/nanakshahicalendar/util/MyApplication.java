6e7c401a575584cdde0327ef2045a35153715119
Sarabjeet Singh <sabbispeak@gmail.com>
2017-09-14 23:45:38 +0530

    added crashlytics support

--- app/src/main/java/com/mdgiitr/nanakshahicalendar/util/MyApplication.java
+++ app/src/main/java/com/mdgiitr/nanakshahicalendar/util/MyApplication.java
@@ -4,8 +4,11 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 
 /**
@@ -14,12 +17,16 @@ import io.realm.Realm;
 
 public class MyApplication extends Application {
 
+    private static FirebaseAnalytics mFireBaseAnalytics;
+
     @Override
     public void onCreate() {
 
         super.onCreate();
         Realm.init(this);
         MobileAds.initialize(this, "ca-app-pub-2995010605730030~9551781504");
+        Fabric.with(this, new Crashlytics());
+        mFireBaseAnalytics = FirebaseAnalytics.getInstance(this);
 
     }
 
@@ -27,5 +34,9 @@ public class MyApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+    }
+
+    public static FirebaseAnalytics getFireBaseAnalyticsInstance(){
+        return mFireBaseAnalytics;
     }
 }
