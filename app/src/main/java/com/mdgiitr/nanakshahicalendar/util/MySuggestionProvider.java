6d4a1004c160615b88fe3fca28f2f97a9e8914fe
Sarabjeet Singh <sabbispeak@gmail.com>
2018-10-21 11:58:10 +0530

    proguard issues

--- app/src/main/java/com/mdgiitr/nanakshahicalendar/util/MySuggestionProvider.java
+++ app/src/main/java/com/mdgiitr/nanakshahicalendar/util/MySuggestionProvider.java
@@ -8,7 +8,7 @@ import android.content.SearchRecentSuggestionsProvider;
 
 public class MySuggestionProvider extends SearchRecentSuggestionsProvider {
 
-    public final static String AUTHORITY = "MySuggestionProvider";
+    public final static String AUTHORITY = "com.mdgiitr.nanakshahicalendar.search";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public MySuggestionProvider() {
